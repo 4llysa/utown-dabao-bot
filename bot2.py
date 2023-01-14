e733c1a8b897561cf90b336f51ed54eb8a475ead
4llysa <59839013+4llysa@users.noreply.github.com>
2023-01-14 20:47:19 +0800

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -17,7 +17,7 @@ async def startorder(update: Update, context: ContextTypes.DEFAULT_TYPE):
 		chat_id=update.effective_chat.id,
 		text="Order has been set up")
 	data[user.first_name]={}
-	return
+	return ConversationHandler.END
 
 async def choosestall(update: Update, context: ContextTypes.DEFAULT_TYPE):
 	reply_keyboard = [["Stall 1", "Other"]]
@@ -35,9 +35,16 @@ async def choosedish(update: Update, context: ContextTypes.DEFAULT_TYPE):
 			reply_keyboard,one_time_keyboard=True,input_field_placeholder="What would you like to order?"))
 	return confirmation
 
-async def confirmation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def confirmation(update: Update, context: ContextTypes.DEFAULT_TYPE)
+	user = update.message.from_user
+	order = update.message.text
+	data[user.first_name] = order
 	await context.bot.send_message(chat_id=update.effective_chat.id,
 		text=f"Your order of {update.message.text} is confirmed")
+	return end
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	return ConversationHandler.END
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 	user=update.message.from_user
@@ -48,14 +55,17 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5860683402:AAELSZ23FifRqbdW09ayDeVg8UM6Yvh6w5k').build()
+    start_handler = CommandHandler('startorder', startorder)
     conv_handler = ConversationHandler(
     	entry_points=[CommandHandler("joinorder", choosestall)],
     		states={
     			choosestall: [MessageHandler(filters.Regex("^(Stall 1|Other)$"), choosestall)],
     			choosedish: [MessageHandler(filters.Regex("^(Stall 1|Other)$"), choosedish)],
-    			confirmation: [MessageHandler(filters.Regex("^(Stall 1|Other)$"), confirmation)]
+    			confirmation: [MessageHandler(filters.Regex("^(Stall 1|Other)$"), confirmation)],
+    			end: [MessageHandler(filters.Regex("^(Stall 1|Other)$"), end)]
 	    },
 	    fallbacks=[CommandHandler("cancel", cancel)],)
     application.add_handler(conv_handler)
+    # application.add_handler(startorder)
     application.run_polling()
 
